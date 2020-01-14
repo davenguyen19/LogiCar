9c2f8052c74760e395c6cfe99d31edd3ce79d80b
davenguyen19 <davenguyen19@users.noreply.github.com>
2020-01-14 13:53:34 -0800

    update

---
+++ PIXY2/LogicarLED/test.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include "NUC123.h"
+
+#define TEST_COUNT 16
+
+uint32_t g_au32SourceData[TEST_COUNT];
+uint32_t g_au32DestinationData[TEST_COUNT];
+volatile uint32_t g_u32TxDataCount;
+volatile uint32_t g_u32RxDataCount;
+
+/* Function prototype declaration */
+void SPI_Init(void);
+
+void SYS_Init(void)
+{
+    /* ENABLING EXTERNAL CLOCK */
+    SYS->GPF_MFP &= ~(SYS_GPF_MFP_PF0_Msk | SYS_GPF_MFP_PF1_Msk);
+    SYS->GPF_MFP |= SYS_GPF_MFP_PF0_XT1_OUT | SYS_GPF_MFP_PF1_XT1_IN;
+	
+	  /* Enable external 12 MHz XTAL */
+    CLK_EnableXtalRC(CLK_PWRCON_XTL12M_EN_Msk);
+	
+	  /* Waiting for clock ready */
+    CLK_WaitClockReady(CLK_CLKSTATUS_XTL12M_STB_Msk);
+	
+    /* Switch HCLK clock source to HXT and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLK_S_HXT, CLK_CLKDIV_HCLK(1));
+	
+	   /* Select HXT as the clock source of UART0 */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART_S_HXT, CLK_CLKDIV_UART(1));
+	
+    /* Select HCLK as the clock source of SPI0 */
+    CLK_SetModuleClock(SPI0_MODULE, CLK_CLKSEL1_SPI0_S_HCLK, MODULE_NoMsk);
+	
+	  /* Enable UART peripheral clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+		
+    /* Enable SPI0 peripheral clock */
+    CLK_EnableModuleClock(SPI0_MODULE);
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate PllClock, SystemCoreClock and CyclesPerUs automatically. */
+    SystemCoreClockUpdate();
+		
+		/* Set PB multi-function pins for UART0 RXD and TXD */
+    SYS->GPB_MFP &= ~(SYS_GPB_MFP_PB0_Msk | SYS_GPB_MFP_PB1_Msk);
+    SYS->GPB_MFP |= (SYS_GPB_MFP_PB0_UART0_RXD | SYS_GPB_MFP_PB1_UART0_TXD);
+		
+    /* Setup SPI0 multi-function pins */
+    SYS->GPC_MFP &= ~(SYS_GPC_MFP_PC0_Msk | SYS_GPC_MFP_PC1_Msk | SYS_GPC_MFP_PC2_Msk | SYS_GPC_MFP_PC3_Msk);
+    SYS->GPC_MFP |= SYS_GPC_MFP_PC0_SPI0_SS0 | SYS_GPC_MFP_PC1_SPI0_CLK | SYS_GPC_MFP_PC2_SPI0_MISO0 | SYS_GPC_MFP_PC3_SPI0_MOSI0;
+    SYS->ALT_MFP &= ~(SYS_ALT_MFP_PC0_Msk | SYS_ALT_MFP_PC1_Msk | SYS_ALT_MFP_PC2_Msk | SYS_ALT_MFP_PC3_Msk);
+    SYS->ALT_MFP |= SYS_ALT_MFP_PC0_SPI0_SS0 | SYS_ALT_MFP_PC1_SPI0_CLK | SYS_ALT_MFP_PC2_SPI0_MISO0 | SYS_ALT_MFP_PC3_SPI0_MOSI0;
+}
+
+void UART0_Init()
+{
+    /* Reset UART0 */
+    SYS->IPRSTC2 |=  SYS_IPRSTC2_UART0_RST_Msk;
+    SYS->IPRSTC2 &= ~SYS_IPRSTC2_UART0_RST_Msk;
+
+    /* Configure UART0 and set UART0 Baudrate */
+	  UART0->BAUD = UART_BAUD_MODE2 | UART_BAUD_MODE2_DIVIDER(__HXT, 115200);
+    UART0->LCR = UART_WORD_LEN_8 | UART_PARITY_NONE | UART_STOP_BIT_1;
+}
+
+void SPI_Init(void)
+{
+    /* Configure SPI0 as a slave, clock idle low, 32-bit transaction, drive output on falling clock edge and latch input on rising edge. */
+    /* Configure SPI0 as a low level active device. SPI peripheral clock rate is equal to system clock rate. */
+    SPI_Open(SPI0, SPI_SLAVE, SPI_MODE_0, 32, NULL);
+}
+
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* MAIN function                                                                                           */
+/*---------------------------------------------------------------------------------------------------------*/
+
+void Delay(void) {
+	int i,j;
+	for(i=0; i<200; i++)
+		for(j=0; j<10000/2; j++){
+		}
+		return;
+	}
+
+int main(void)
+{
+    int32_t i32Err;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+    /* Init UART0 for printf */
+    UART0_Init();
+	
+	    /* Init SPI */
+    SPI_Init();
+	
+    /* Enable Dual I/O input mode */
+    SPI_ENABLE_DUAL_INPUT_MODE(SPI0);
+	
+	    /* Enable FIFO mode and set FIFO threshold. */
+    SPI_EnableFIFO(SPI0, 2, 2);
+		
+		printf("+-----------------------------------------------------+\n");
+    printf("|           LogiCar Code Setup Complete               |\n");
+    printf("+-----------------------------------------------------+\n");
+
+	while(1);
+}
